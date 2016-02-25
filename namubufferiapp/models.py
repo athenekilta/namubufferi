a4bee555225f40bfbda86ea7c3904afa1cb4adff
Niklas <Niklas@Tyson.local>
2016-02-26 00:19:05 +0200

    Base.html cleanup, add customfont

--- namubufferiapp/models.py
+++ namubufferiapp/models.py
@@ -16,9 +16,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
 class Category(models.Model):
-  name = models.TextField(unique=True)
+    name = models.TextField(unique=True)
 
 class Product(models.Model):
-  name = models.CharField(max_length=30, unique=True)
-  category = models.ForeignKey(Category, related_name='products')
-  price = models.FloatField(default=0)
+    name = models.CharField(max_length=30, unique=True)
+    category = models.ForeignKey(Category, related_name='products')
+    price = models.FloatField(default=0)

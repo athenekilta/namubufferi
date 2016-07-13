d1441a99a119240a773a82f125eadaee152e2016
Olli Angervuori <olli.angervuori@aalto.fi>
2016-07-13 04:33:44 +0300

    Add a nasty way for displaying login errors

--- namubufferiapp/urls.py
+++ namubufferiapp/urls.py
@@ -3,6 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth import views as auth_views
 from namubufferiapp.views import *
 
+global_context = dict(signin_form=AuthenticationForm(),
+                      register_form=UserCreationForm(),
+                      money_form=MoneyForm(),
+                      products=Product.objects.all(),
+                      categories=Category.objects.all(),
+                      message="",
+                      )
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'namubufferi.views.home', name='home'),
@@ -10,7 +18,7 @@ urlpatterns = patterns('',
 
     url(r'^$', cover, name="cover"),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'namubufferiapp/base.html',
-                                                         'extra_context': {'register_form': UserCreationForm}}),
+                                                         'extra_context': global_context}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/', }),
     url(r'^register/$', register),
     url(r'^buy/', buy_view, name="buy_view"),

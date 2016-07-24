dca0e7825ff2f4e22ff3238e38671ebccd5670ea
Olli Angervuori <olli.angervuori@aalto.fi>
2016-07-24 07:21:33 +0300

    Add product/buy modal

--- namubufferiapp/static/namubufferiapp/js/namubufferi.js
+++ namubufferiapp/static/namubufferiapp/js/namubufferi.js
@@ -1,5 +1,17 @@
-$( document ).ready(function (){
+
+$(document).ready(function() {
+    'use strict';
     $('#search').hideseek();
     $(".product").fitText();
-    $('#receiptModal').modal();
+    $('#receiptModal').modal();    
+    $('#productModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget); // Button that triggered the modal
+      var productkey = button.data('productkey'); // Extract info from data-* attributes
+      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+      var modal = $(this);
+      modal.find('#product-modal-name').text(button.data('productname'));
+      modal.find('#product-modal-pk').val(button.data('productkey'));
+      modal.find('#product-modal-price').text(button.data('productprice'));
+    });
 });

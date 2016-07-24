0ae72d2d3b7214e533d311281c2a432ce608bb66
Olli Angervuori <olli.angervuori@aalto.fi>
2016-07-24 20:36:33 +0300

    Move ajax csrf token to a separate file

--- namubufferiapp/static/namubufferiapp/js/namubufferi.js
+++ namubufferiapp/static/namubufferiapp/js/namubufferi.js
@@ -23,35 +23,7 @@ function getTransactionHistory() {
 
 $(document).ready(function() {
     'use strict';
-
-    // https://docs.djangoproject.com/en/1.9/ref/csrf/#ajax
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
+    
     // https://api.jquery.com/jquery.post/
     // Attach a submit handler to the form
     $( "#buy-form" ).submit(function( event ) {

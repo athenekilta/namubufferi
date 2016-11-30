43a8efd5728e6d29acc733a64c0adcbc294bdfee
Olli Angervuori <olli.angervuori@gmail.com>
2016-11-30 19:17:17 +0200

    Refactor

--- namubufferiapp/urls.py
+++ namubufferiapp/urls.py
@@ -1,28 +1,24 @@
-from django.conf.urls import include, url
-
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 
+from namubufferiapp import views
 from namubufferiapp.forms import MagicAuthForm
-
-from namubufferiapp.views import home_view, register_view, buy_view, deposit_view
-from namubufferiapp.views import cancel_transaction_view, receipt_view, transaction_history_view
-from namubufferiapp.views import magic_auth_view
 
 
 urlpatterns = [
-    url(r'^$', home_view, name="home"),
+    url(r'^$', views.home, name="home"),
     url(r'^login', auth_views.login, {'template_name': 'namubufferiapp/base_magiclogin.html',
                                                        'extra_context': {'register_form': UserCreationForm(),
                                                                          'magic_auth_form': MagicAuthForm(),
                                                                          }}),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}),
-    url(r'^register/$', register_view, name="register"),
-    url(r'^buy/$', buy_view, name="buy"),
-    url(r'^deposit/$', deposit_view, name="deposit"),
-    url(r'^cancel/$', cancel_transaction_view, name="cancel"),
-    url(r'^receipt/$', receipt_view, name="receipt"),
-    url(r'^history/$', transaction_history_view, name="history"),
-    url(r'^magic/$', magic_auth_view),
-    url(r'^magic/(?P<magic_token>.*)/$', magic_auth_view, name="magic"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^buy/$', views.buy, name="buy"),
+    url(r'^deposit/$', views.deposit, name="deposit"),
+    url(r'^cancel/$', views.cancel_transaction, name="cancel"),
+    url(r'^receipt/$', views.receipt, name="receipt"),
+    url(r'^history/$', views.transaction_history, name="history"),
+    url(r'^magic/$', views.magic_auth),
+    url(r'^magic/(?P<magic_token>.*)/$', views.magic_auth, name="magic"),
 ]

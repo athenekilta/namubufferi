0e69f767fce7d49218f77e9263882ced0731559a
Sami-Petteri Pukkila <sami.pukkila@gmail.com>
2017-10-30 23:49:41 +0200

    remove heroku stuff
    probably never going to use these in our context

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 python3 manage.py migrate
+./node_modules/.bin/webpack --config /namubufferi/webpack.config.js
+python3 manage.py collectstatic --noinput
--- namubufferi/settings.py
+++ namubufferi/settings.py
@@ -157,7 +157,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Simplified static file serving.

da17c1b87c25d08e3686af9607acff281df5cde9
Sami-Petteri Pukkila <sami.pukkila@gmail.com>
2018-02-02 22:02:37 +0200

    Allow negative deposits

--- namubufferiapp/forms.py
+++ namubufferiapp/forms.py
@@ -4,9 +4,9 @@ from .models import Product, Category
 
 class MoneyForm(forms.Form):
     #amount = forms.DecimalField(max_digits=5, decimal_places=1, widget=forms.NumberInput(attrs={'pattern': '[0-9]*'}))
-    euros = forms.IntegerField(min_value=0,
+    euros = forms.IntegerField(min_value=-100,
                                max_value=100,
-                               widget=forms.NumberInput(attrs={'pattern': '[0-9]*',
+                               widget=forms.NumberInput(attrs={'pattern': '\-?[0-9]*',
                                                                'class': 'form-control',
                                                                'placeholder': '00',
                                                                'autofocus': '',

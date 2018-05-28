8d59e96cd47d3b7944a2f93cf77d60c92348a577
Sami "mylvari" Pukkila <sami.pukkila@gmail.com>
2018-05-28 19:38:48 +0300

    Modify user information in admin panel to be sensible

--- namubufferiapp/admin.py
+++ namubufferiapp/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
-from .models import Category, Product, Transaction, UserTag, ProductTag
+from .models import Category, Product, Transaction, UserTag, ProductTag, Account
+
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'Account'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (AccountInline, )
+    list_display = ('email', 'is_staff', 'is_superuser', 'last_login', 'balance')
+    list_select_related = ('account', )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+    def balance(self, instance):
+        return instance.account.balance
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Product)
 admin.site.register(Category)

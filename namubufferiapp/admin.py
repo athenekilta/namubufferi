40efdfaf78696cbb6a8db8584c77201895850998
Sami "mylvari" Pukkila <sami.pukkila@gmail.com>
2018-05-28 19:48:17 +0300

    Add filtering by balance to admin panel

--- namubufferiapp/admin.py
+++ namubufferiapp/admin.py
@@ -11,10 +11,31 @@ class AccountInline(admin.StackedInline):
     verbose_name_plural = 'Account'
     fk_name = 'user'
 
+class BalanceListFilter(admin.SimpleListFilter):
+    title = 'balance'
+    parameter_name = 'balance'
+    default_value = None
+
+    def lookups(self, request, model_admin):
+        return [
+            ("negative", "negative balances only")
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "negative":
+            negative_users = []
+            for user in queryset:
+                if user.account.balance < 0:
+                    negative_users.append(user.id)
+            return User.objects.filter(id__in=negative_users)
+
+        return queryset
+
 class CustomUserAdmin(UserAdmin):
     inlines = (AccountInline, )
     list_display = ('email', 'is_staff', 'is_superuser', 'last_login', 'balance')
     list_select_related = ('account', )
+    list_filter = UserAdmin.list_filter + (BalanceListFilter, )
 
     def get_inline_instances(self, request, obj=None):
         if not obj:

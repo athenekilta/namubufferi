112b787661314bd1a6518d508786a462ce784bb3
Joel Juhela <joel.juhela@bluugo.fi>
2022-08-22 19:45:52 +0300

    Add password reset and login templates

--- namubufferi/urls.py
+++ namubufferi/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
                 path("admin/", admin.site.urls),
                 path("!/", include("singlepageapp.urls")),
                 path("terms/", include("terms.urls")),
-                path("users/", include("users.urls")),
+                path("accounts/", include("django.contrib.auth.urls")),
             ],
         ),
     ),

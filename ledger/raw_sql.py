9c4570fa38cfbdfb1af3390069c6b1f932e6b171
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2024-01-28 13:43:31 +0200

    fix: only return negative debts

--- ledger/raw_sql.py
+++ ledger/raw_sql.py
@@ -3,23 +3,26 @@ from django.http import JsonResponse
 
 def get_debts():
     
-    query = """SELECT 
-                  uu.id AS user_id,
-                  uu.username,
-                  uu.first_name,
-                  uu.last_name,
-                  SUM(lt.price * lt.quantity) AS total_paid
-              FROM 
-                  users_user uu
-              INNER JOIN 
-                  ledger_account la ON uu.id = la.user_id
-              INNER JOIN 
-                  ledger_transaction lt ON la.id = lt.account_id
-              GROUP BY 
-                  uu.id, uu.username, uu.first_name, uu.last_name
-              ORDER BY 
-                  total_paid ASC
-              LIMIT 5;
+    query = """
+            SELECT 
+                uu.id AS user_id,
+                uu.username,
+                uu.first_name,
+                uu.last_name,
+                SUM(lt.price * lt.quantity) AS total_paid
+            FROM 
+                users_user uu
+            INNER JOIN 
+                ledger_account la ON uu.id = la.user_id
+            INNER JOIN 
+                ledger_transaction lt ON la.id = lt.account_id
+            GROUP BY 
+                uu.id, uu.username, uu.first_name, uu.last_name
+            HAVING 
+                SUM(lt.price * lt.quantity) < 0
+            ORDER BY 
+                total_paid ASC
+            LIMIT 5;
             """
 
     with connection.cursor() as cursor:
